0f7c789986d1cbd8ae8a0c32bb9e2542945b2e55
pujan <pujxnp@gmail.com>
2023-05-01 10:43:52 +0530

    Added Auth0 provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/components/App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <Auth0Provider
+      domain="dev-5nxmvrndnoavntp3.us.auth0.com"
+      clientId="qYOcSG98SRhJ08SdDUISFFmUgDmnQaAa"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <Router>
+        <App />
+      </Router>
+    </Auth0Provider>
 
+  </React.StrictMode>
 )
