22b8c8955ba237ea4918a35ea3e0fc5579724bdd
pujan <pujxnp@gmail.com>
2023-05-01 11:39:01 +0530

    Added login and logout using Auth0

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import JoinGameBox from "@/components/JoinGameBox";
 import CreateGameBox from "@/components/CreateGameBox";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 const LandingPage = () => {
+
+    const { loginWithRedirect, logout } = useAuth0();
 
     const [mode, setMode] = useState("");
 
@@ -17,6 +20,8 @@ const LandingPage = () => {
 
     return (
         <>
+            <button onClick={loginWithRedirect}>Login</button>
+            <button onClick={logout}>Logout</button>
             {mode != "join" && <button onClick={handleJoinGame}>Join Game</button>}
             {mode != "create" && <button onClick={handleCreateGame}>Create Game</button>}
             {
