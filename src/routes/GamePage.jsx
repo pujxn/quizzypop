a463c872cdddac84563b01e0696ee5816ce6fda8
pujan <pujxnp@gmail.com>
2023-05-02 15:02:04 +0530

    Fetching data from API, and able to pulll in question on GamePage load. So proud of you for patiently reading the docs, playing arpunmd on CodeSandbox and attempting this in a fun way. Keep it up! :)

--- src/routes/GamePage.jsx
+++ src/routes/GamePage.jsx
@@ -1,5 +1,59 @@
+import { useState, useEffect } from "react";
+
 const GamePage = () => {
-    return <h2>Current Game</h2>
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        let sessionToken = "";
+
+        fetch("https://opentdb.com/api_token.php?command=request")
+            .then(
+                res => res.json()
+            )
+            .catch((e) => {
+                console.log(e);
+            })
+            .then(res => {
+                // console.log(res)
+                sessionToken = res.token;
+                // console.log("Session Token", sessionToken)
+                // return res;
+            })
+            .then(
+                res =>
+                    fetch(`https://opentdb.com/api.php?amount=10&token=${sessionToken}`)
+
+            ).
+            then(
+                res => res.json()
+            )
+            .catch((e) => {
+                console.log(e);
+            })
+            .then(
+                res => {
+                    setQuestions([...res.results]);
+                    setIsLoading(false);
+                }
+            )
+    }, [])
+
+
+    console.log(questions);
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <>
+            {questions.map((ele, idx) =>
+                <p key={idx}>{ele.question}</p>
+            )}
+        </>
+    )
 }
 
 export default GamePage;
